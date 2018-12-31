26841502516ca5e4076d115dcd75bb392cf647b0
Shunsuke Yamazaki <>
2018-12-31 21:40:54 +0900

    Set config.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-4.times do |i|
-  a = Article.create!(status: :draft)
+body = <<~EOS
+  "春はあけぼの。やうやう（ようよう）白くなりゆく山ぎは（やまぎわ）、
+  少し明（あ）かりて、紫（むらさき）だちたる雲の細くたなびきたる。"
+EOS
 
-  section = Section.new(body: "春はあけぼの。やうやう（ようよう）白くなりゆく山ぎは（やまぎわ）、少し明（あ）かりて、紫（むらさき）だちたる雲の細くたなびきたる。", article: a)
+config = {
+  width: 2,
+  height: 1,
+}
+
+4.times do |i|
+  a = Article.new(status: :draft)
+
+  section = Section.new(body: body, article: a)
   section.save!
+
+  config[:layout] = [
+    {s: section.id},
+    {i: nil}
+  ]
+
+  a.config = config.to_json
+  a.save!
 end
