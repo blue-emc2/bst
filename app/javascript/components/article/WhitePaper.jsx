8d5ad7bea31013b46db7aa32006bd0d609b19645
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-01-09 20:25:21 +0900

    Swap the input content text or image. タブによる切り替え

--- app/javascript/components/article/WhitePaper.jsx
+++ app/javascript/components/article/WhitePaper.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -11,6 +12,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Dropzone from 'react-dropzone'
 
 const styles = theme => ({
   root: {
@@ -35,23 +37,80 @@ const styles = theme => ({
   },
 })
 
-function TabContainer(props) {
-  const { value } = props;
+class ImageDropZone extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isUploading: false,
+      images: []
+    };
 
-  return (
-    <TextField
-      id="standard-name"
-      multiline
-      rowsMax="10"
-      margin="normal"
-    />
-  );
+    this.handleOnDrop = this.handleOnDrop.bind(this);
+  }
+
+  handleOnDrop(files) {
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>React S3 Image Uploader Sample</h1>
+        <Dropzone onDrop={this.handleOnDrop} accept="image/*">
+           {({getRootProps, getInputProps}) => (
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+                <p>Drop files here, or click to select files</p>
+            </div>
+          )}
+        </Dropzone>
+        {this.state.isUploading ?
+          <div>ファイルをアップロードしています</div> :
+          <div>ここに画像をドラックまたはクリック</div>}
+        {this.state.images.length > 0 &&
+          <div style={{margin: 30}}>
+            {this.state.images.map(({name, url}) =>
+              <img key={name} src={url} style={{width: 200, height: 200}}/>)}
+          </div>}
+      </div>
+    );
+  }
 }
+
+class TabContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderTextOrImage(type) {
+    if (type === 'text') {
+      return <TextField
+        id="standard-name"
+        multiline
+        rowsMax="10"
+        margin="normal"
+      />
+    } else {
+      return <ImageDropZone />
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.renderTextOrImage(this.props.type)}
+      </React.Fragment>
+    )
+  }
+}
+
+TabContainer.propTypes = {
+  type: PropTypes.string.isRequired,
+};
 
 class WhitePaper extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: 0};
+    this.state = {value: 'text'};
   }
 
   handleChange = (event, value) => {
@@ -70,12 +129,11 @@ class WhitePaper extends React.Component {
             <Grid container spacing={40} >
               <Grid item xs={6} container alignItems="center" justify="center">
                 <Tabs value={value} onChange={this.handleChange}>
-                  <Tab label="文章に入力する" />
-                  <Tab label="画像をアップロードする" />
+                  <Tab value='text' label="文章に入力する" />
+                  <Tab value='image' label="画像をアップロードする" />
                 </Tabs>
                 <Card className={classes.card}>
-                  {value === 0 && <TabContainer>value</TabContainer>}
-                  {value === 1 && <TabContainer>value</TabContainer>}
+                  <TabContainer type={value} />
                 </Card>
               </Grid>
               <Grid item xs={6} container alignItems="center" justify="center">
