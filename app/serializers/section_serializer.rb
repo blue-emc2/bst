8aacf0d85c2b7f6d8b5378835c6b143307422055
blue-emc2 <>
2019-03-06 23:55:43 +0900

    アップロードした画像URLを取得する

--- app/serializers/section_serializer.rb
+++ app/serializers/section_serializer.rb
@@ -1,5 +1,11 @@
 class SectionSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+
   attributes :id, :body, :photo
 
   belongs_to :article
+
+  def photo
+    rails_blob_url(object.photo) if object.photo.attached?
+  end
 end
